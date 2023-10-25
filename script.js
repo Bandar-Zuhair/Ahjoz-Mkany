4b8f55c10f934884a5a048d34029b410c1b7c531
Bandar-Zuhair <146974861+Bandar-Zuhair@users.noreply.github.com>
2023-10-25 03:57:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -145,112 +145,70 @@ var villaDetailsCipanas = [
 
 
 
+
+
+
 /* Scroll Down */
-page_Scroll = function () {
+page_Scroll = () => {
     var goDownToVillaDetails = document.getElementById("villa_Details_Text_Contact_Us");
     setTimeout(function () {
-            goDownToVillaDetails.scrollIntoView();
+        goDownToVillaDetails.scrollIntoView();
     }, 500);
 };
-
-
-
 /* Booking Method */
-booking_Method.onclick = function () {
+booking_Method.onclick = () => {
     var arMethod = "1- اختار الفيلا الي تبغاها\n2- احفظ رقم الفيلا ومكانها واضغط على احجز الان\n3- ارسل رقم الفيلا ومكانها واسم اليوم او رقم التاريخ الي حاب تحجز فيه الفيلا عشان تشوف اذا كانت متاحه ولا لا\n4- الدفع سيكون مباشر وعند الحجز فقط"
     var enMethod = "\n\n1- Pick the villa the want\n2- Remember the villa id and area\n3- Click on book now and send the villa number with it's area\n4- Send the name of the day or the number of a date to see if the villa you want is avalible or no\n5- The payment will be direct and only upon booking"
     alert(arMethod + enMethod);
 };
-
-
-
 /* Note */
-note_Btn.onclick = function () {
+note_Btn.onclick = () => {
     var arNote = "المشكلة الوحيدة الي راح تواجها في موقع احجز مكاني هي ان في بعض الاحيان تكون الفيلا الي تريد حجزها بالفعل محجوزة من شخص اخر، ف افضل حل لهذه المشكلة هو حجز اكثر من فيلا واحدة لكي تضمن مكانك في حال ان كانت الفلل الي اخترتها محجوزة بالفعل وواحدة منها تكون متاحة لك";
     var enNote = "\n \nThe only problem that you might face in Ahjoz Mkany website is that sometimes the Villa you want is already taken by someone else, therfore The best solution to this problem is that you can book more than one Villa just in case if they were all taken then one of them might be available for you";
     alert(arNote + enNote);
 };
-
-
-
 /* Hide & Show Header */
-hide_Btn.onclick = function () {
+hide_Btn.onclick = () => {
     header.classList.add("hide_Display");
     hide_Btn.classList.add("hide_Display");
     show_Btn.classList.remove("hide_Display");
 };
-show_Btn.onclick = function () {
+show_Btn.onclick = () => {
     header.classList.remove("hide_Display");
     hide_Btn.classList.remove("hide_Display");
     show_Btn.classList.add("hide_Display");
 };
+/* Create Cards */
+createVillaCard = (data, areaId, areaName) => {
+    data.forEach((item, i) => {
+        const { cardNum, id, image, location, price } = item;
+        const villaCard = document.createElement("div");
+        villaCard.className = "villa_card";
 
-
-
-
-
-
-
-/* Create Cisarua Cards */
-cardDataCisarua.forEach((element, i) => {
-    var villa_Card = document.createElement("div");
-    villa_Card.classList.add("villa_card");
-    var card_Info = `
-    <div onclick="show_Villa_Details('سيساروا', ${cardDataCisarua[i].cardNum}, '${cardDataCisarua[i].id}'); page_Scroll();" id="${cardDataCisarua[i].id}" class="villa_Card">
-        <img src="${cardDataCisarua[i].image}" alt="احجز مكاني في فلل اندونيسيا" title="احجز مكاني في فلل اندونيسيا">
-        <div class="villa_Info">
-            <h1>${cardDataCisarua[i].location}</h1>
-            <h1 class="price">${cardDataCisarua[i].price}</h1>
+        villaCard.innerHTML = `
+        <div onclick="show_Villa_Details('${areaName}', ${cardNum}, '${id}'); page_Scroll();" id="${id}" class="villa_Card">
+          <img src="${image}" alt="احجز مكاني في فلل اندونيسيا" title="احجز مكاني في فلل اندونيسيا">
+          <div class="villa_Info">
+            <h1>${location}</h1>
+            <h1 class="price">${price}</h1>
             <h1 class="villa_id">رقم الفيلا ${i + 1}</h1>
+          </div>
         </div>
-    </div>
-    `;
-    villa_Card.innerHTML = card_Info;
-    cisarua_villa_Area_Id.appendChild(villa_Card);
-});
-/* Create Cipanas Cards */
-cardDataCipanas.forEach((element, i) => {
-    var villa_Card = document.createElement("div");
-    villa_Card.classList.add("villa_card");
-    var card_Info = `
-    <div onclick="show_Villa_Details('شيباناس', ${cardDataCipanas[i].cardNum}, '${cardDataCipanas[i].id}'); page_Scroll();" id="${cardDataCipanas[i].id}" class="villa_Card">
-        <img src="${cardDataCipanas[i].image}" alt="احجز مكاني في فلل اندونيسيا" title="احجز مكاني في فلل اندونيسيا">
-        <div class="villa_Info">
-            <h1>${cardDataCipanas[i].location}</h1>
-            <h1 class="price">${cardDataCipanas[i].price}</h1>
-            <h1 class="villa_id">رقم الفيلا ${i + 1}</h1>
-        </div>
-    </div>
-    `;
-    villa_Card.innerHTML = card_Info;
-    cipanas_villa_Area_Id.appendChild(villa_Card);
-});
+      `;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        areaId.appendChild(villaCard);
+    });
+}
+createVillaCard(cardDataCisarua, cisarua_villa_Area_Id, 'سيساروا');
+createVillaCard(cardDataCipanas, cipanas_villa_Area_Id, 'شيباناس');
 /* Show Cards Details */
-show_Villa_Details = function (areaLocation, cardNum, id) {
+show_Villa_Details = (areaLocation, cardNum, id) => {
     var imgNum = 0;
     var areaSelected = areaLocation === 'سيساروا' ? villaDetailsCisarua : villaDetailsCipanas;
     var villa_Details_Card = document.createElement("div");
     villa_Details_Card.classList.add("villa_Details_Card");
 
-    updateVillaDetailsInfo = function () {
+    updateVillaDetailsInfo = () => {
         var villa = areaSelected[cardNum];
 
         var villa_Details_Info = `
@@ -282,20 +240,20 @@ show_Villa_Details = function (areaLocation, cardNum, id) {
         villa_Details_Area.innerHTML = "";
         villa_Details_Area.appendChild(villa_Details_Card);
 
-        take_Me_Back = function(id) {
+        take_Me_Back = function (id) {
             var targetedCard = document.getElementById(id);
 
             if (targetedCard) {
-            var elementTop = targetedCard.getBoundingClientRect().top;
-            var offset = elementTop - (window.innerHeight / 2 - targetedCard.clientHeight / 2);
-            window.scrollBy({
-                top: offset,
-            });
-            targetedCard.style.backgroundColor = "green";
-            targetedCard.style.borderColor = "orange";
+                var elementTop = targetedCard.getBoundingClientRect().top;
+                var offset = elementTop - (window.innerHeight / 2 - targetedCard.clientHeight / 2);
+                window.scrollBy({
+                    top: offset,
+                });
+                targetedCard.style.backgroundColor = "green";
+                targetedCard.style.borderColor = "orange";
             }
         }
-        photo_Villa_Details_Btn.onclick = function () {
+        photo_Villa_Details_Btn.onclick = () => {
             villa_Details_Video_Area.classList.add("hide_Display");
             villa_Details_Video.classList.add("hide_Display");
             villa_Details_Img_Area.classList.remove("hide_Display");
@@ -305,7 +263,7 @@ show_Villa_Details = function (areaLocation, cardNum, id) {
         };
 
         if (video_Villa_Details_Btn) {
-            video_Villa_Details_Btn.onclick = function () {
+            video_Villa_Details_Btn.onclick = () => {
                 villa_Details_Video_Area.classList.remove("hide_Display");
                 villa_Details_Video.classList.remove("hide_Display");
                 villa_Details_Img_Area.classList.add("hide_Display");
@@ -315,12 +273,12 @@ show_Villa_Details = function (areaLocation, cardNum, id) {
             };
         }
 
-        arrow_Forward.onclick = function () {
+        arrow_Forward.onclick = () => {
             imgNum = (imgNum + 1) % villa.image.length;
             villa_Details_Full_Screen_Image.src = villa.image[imgNum];
         };
 
-        arrow_Back.onclick = function () {
+        arrow_Back.onclick = () => {
             imgNum = imgNum - 1;
             if (imgNum === -1) {
                 imgNum = villa.image.length - 1;
@@ -329,7 +287,7 @@ show_Villa_Details = function (areaLocation, cardNum, id) {
         };
 
 
-        villa_Details_Full_Screen_Image.onclick = function () {
+        villa_Details_Full_Screen_Image.onclick = () => {
             Full_Screen_Image(villa_Details_Full_Screen_Image.src);
             var overlay_Layer = document.createElement("div");
             overlay_Layer.classList.add("overlay");
@@ -339,7 +297,7 @@ show_Villa_Details = function (areaLocation, cardNum, id) {
 
     updateVillaDetailsInfo();
 
-    Full_Screen_Image = function (src) {
+    Full_Screen_Image = (src) => {
         var fullScreenImage = document.createElement("div");
         fullScreenImage.classList.add("full_Screen_Image");
 
@@ -356,7 +314,7 @@ show_Villa_Details = function (areaLocation, cardNum, id) {
         document.body.appendChild(fullScreenImage);
     };
 
-    closeFullScreenImage = function () {
+    closeFullScreenImage = () => {
         var fullScreenImage = document.querySelector(".full_Screen_Image");
         if (fullScreenImage) {
             document.body.removeChild(fullScreenImage);
@@ -369,86 +327,82 @@ show_Villa_Details = function (areaLocation, cardNum, id) {
 
     window.addEventListener("scroll", closeFullScreenImage);
 }
-
-
-
-
-
-
 /* Create Comment Card */
 var commentNum = 0;
-var comment_Card = document.createElement("div");
-comment_Card.classList.add("comment_Card");
+var comment_Card = createAndAppend("div", "comment_Card", all_Comments_Area);
 var fullScreenImage = null;
 var overlay = null;
-updateCommentInfo = function () {
+
+function createAndAppend(tag, className, parent) {
+    var element = document.createElement(tag);
+    element.classList.add(className);
+    parent.appendChild(element);
+    return element;
+}
+
+updateCommentInfo = () => {
     var comment_Info = `
-    <div id="comment_Card">
-        <div id="comment_Image">
-            <ion-icon id="sec2_Arrow_Forward" class="arrow_Icon" name="arrow-forward-outline"></ion-icon>
-            <img id="comment_Info_Image" src=${commentData[commentNum].image} alt="تعليقات موقع احجز مكاني" title="تعليقات موقع احجز مكاني">
-            <ion-icon id="sec2_Arrow_Back" class="arrow_Icon" name="arrow-back-outline"></ion-icon>
+        <div id="comment_Card">
+            <div id="comment_Image">
+                <ion-icon id="sec2_Arrow_Forward" class="arrow_Icon" name="arrow-forward-outline"></ion-icon>
+                <img id="comment_Info_Image" src="${commentData[commentNum].image}" alt="تعليقات موقع احجز مكاني" title="تعليقات موقع احجز مكاني">
+                <ion-icon id="sec2_Arrow_Back" class="arrow_Icon" name="arrow-back-outline"></ion-icon>
+            </div>
+            <div id="my_Reply">
+                <h1>${commentData[commentNum].reply}</h1>
+            </div>
         </div>
-        <div id="my_Reply">
-            <h1>${commentData[commentNum].reply}</h1>
-        </div>
-    </div>
-  `;
+    `;
 
     comment_Card.innerHTML = comment_Info;
-    all_Comments_Area.innerHTML = "";
-    all_Comments_Area.appendChild(comment_Card);
 
-    sec2_Arrow_Forward.onclick = function () {
+    var sec2_Arrow_Forward = document.getElementById("sec2_Arrow_Forward");
+    var sec2_Arrow_Back = document.getElementById("sec2_Arrow_Back");
+    var comment_Info_Image = document.getElementById("comment_Info_Image");
+
+    sec2_Arrow_Forward.onclick = () => {
         commentNum = (commentNum + 1) % commentData.length;
-        updateCommentInfo();
+        updateImageAndReply();
     };
 
-    sec2_Arrow_Back.onclick = function () {
+    sec2_Arrow_Back.onclick = () => {
         commentNum = (commentNum - 1 + commentData.length) % commentData.length;
-        updateCommentInfo();
+        updateImageAndReply();
     };
 
-    comment_Info_Image.onclick = function () {
+    comment_Info_Image.onclick = () => {
         openFullScreenImage(commentData[commentNum].image);
     };
 
-    openFullScreenImage = function (src) {
-        fullScreenImage = document.createElement("div");
-        fullScreenImage.classList.add("full_Screen_Image");
-
+    openFullScreenImage = (src) => {
+        fullScreenImage = createAndAppend("div", "full_Screen_Comment", document.body);
         var fullScreenImageInfo = `
-      <img src="${src}" alt="احجز مكاني في فلل اندونيسيا">
-    `;
-
+            <img src="${src}" alt="احجز مكاني في فلل اندونيسيا">
+        `;
         fullScreenImage.innerHTML = fullScreenImageInfo;
 
-        overlay = document.createElement("div");
-        overlay.classList.add("overlay");
+        overlay = createAndAppend("div", "overlay", document.body);
 
-        document.body.appendChild(fullScreenImage);
-        document.body.appendChild(overlay);
+        fullScreenImage.onclick = closeFullScreenImage;
+        overlay.onclick = closeFullScreenImage;
 
-        fullScreenImage.onclick = function () {
-            closeFullScreenImage();
-        };
-
-        overlay.onclick = function () {
-            closeFullScreenImage();
-        };
-
-        window.addEventListener("scroll", function () {
-            closeFullScreenImage();
-        });
+        window.addEventListener("scroll", closeFullScreenImage);
     }
 
-    closeFullScreenImage = function () {
+    closeFullScreenImage = () => {
         if (fullScreenImage) {
             document.body.removeChild(fullScreenImage);
-            document.body.removeChild(overlay);
             fullScreenImage = null;
+        }
+        if (overlay) {
+            document.body.removeChild(overlay);
             overlay = null;
         }
+    }
+
+    updateImageAndReply = () => {
+        comment_Info_Image.src = commentData[commentNum].image;
+        my_Reply.querySelector('h1').textContent = commentData[commentNum].reply;
     }
 }
 updateCommentInfo();
